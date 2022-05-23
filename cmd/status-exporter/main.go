0dd33cdd7573a53b210c6137833cf8013638690b
Guy Shaibi <guy.shaibi@run.ai>
2022-05-23 09:02:46 +0300

    main to app

--- cmd/status-exporter/main.go
+++ cmd/status-exporter/main.go
@@ -2,45 +2,12 @@ package main
 
 import (
 	"log"
-	"os"
-	"os/signal"
-	"syscall"
 
-	"github.com/run-ai/gpu-mock-stack/internal/common/config"
-	"github.com/run-ai/gpu-mock-stack/internal/status-exporter/export"
-	"github.com/run-ai/gpu-mock-stack/internal/status-exporter/export/labels"
-	"github.com/run-ai/gpu-mock-stack/internal/status-exporter/export/metrics"
-	"github.com/run-ai/gpu-mock-stack/internal/status-exporter/watch"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
+	status_exporter "github.com/run-ai/gpu-mock-stack/internal/status-exporter"
 )
 
 func main() {
 	log.Println("Fake Status Exporter Running")
 
-	requiredEnvVars := []string{"NODE_NAME", "TOPOLOGY_CM_NAME", "TOPOLOGY_CM_NAMESPACE"}
-	config.ValidateConfig(requiredEnvVars)
-
-	config, err := rest.InClusterConfig()
-	if err != nil {
-		panic(err.Error())
-	}
-	kubeclient := kubernetes.NewForConfigOrDie(config)
-
-	// Watch for changes, and export metrics
-	stopper := make(chan struct{})
-	var watcher watch.Interface = watch.NewKubeWatcher(kubeclient)
-	var metricExporter export.Interface = metrics.NewMetricsExporter(watcher)
-	var labelsExporter export.Interface = labels.NewLabelsExporter(watcher, kubeclient)
-
-	go watcher.Watch(stopper)
-	go metricExporter.Run(stopper)
-	go labelsExporter.Run(stopper)
-
-	sig := make(chan os.Signal, 1)
-	signal.Notify(sig, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
-
-	s := <-sig
-	log.Printf("Received signal \"%v\"\n", s)
-	close(stopper)
+	status_exporter.Run()
 }
---
+++ internal/status-exporter/app.go
@@ -0,0 +1,49 @@
+package status_exporter
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/run-ai/gpu-mock-stack/internal/common/config"
+	"github.com/run-ai/gpu-mock-stack/internal/status-exporter/export"
+	"github.com/run-ai/gpu-mock-stack/internal/status-exporter/export/labels"
+	"github.com/run-ai/gpu-mock-stack/internal/status-exporter/export/metrics"
+	"github.com/run-ai/gpu-mock-stack/internal/status-exporter/watch"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+)
+
+var InClusterConfigFn = rest.InClusterConfig
+var KubeClientFn = func(c *rest.Config) kubernetes.Interface {
+	return kubernetes.NewForConfigOrDie(c)
+}
+
+func Run() {
+	requiredEnvVars := []string{"NODE_NAME", "TOPOLOGY_CM_NAME", "TOPOLOGY_CM_NAMESPACE"}
+	config.ValidateConfig(requiredEnvVars)
+
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+	kubeclient := kubernetes.NewForConfigOrDie(config)
+
+	// Watch for changes, and export metrics
+	stopper := make(chan struct{})
+	var watcher watch.Interface = watch.NewKubeWatcher(kubeclient)
+	var metricExporter export.Interface = metrics.NewMetricsExporter(watcher)
+	var labelsExporter export.Interface = labels.NewLabelsExporter(watcher, kubeclient)
+
+	go watcher.Watch(stopper)
+	go metricExporter.Run(stopper)
+	go labelsExporter.Run(stopper)
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+
+	s := <-sig
+	log.Printf("Received signal \"%v\"\n", s)
+	close(stopper)
+}
