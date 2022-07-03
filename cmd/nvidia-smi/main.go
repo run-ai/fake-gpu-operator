9672b4c387e22ea55298139cfdd94599daf4c40f
Guy Shaibi <guy.shaibi@run.ai>
2022-07-03 13:52:04 +0300

    final

--- cmd/nvidia-smi/main.go
+++ cmd/nvidia-smi/main.go
@@ -1,35 +1,18 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
+	"time"
 
 	"github.com/jedib0t/go-pretty/v6/table"
 	"github.com/run-ai/fake-gpu-operator/internal/common/topology"
 )
-
-var nvidiaSmiOutputTemplate = `
-+-----------------------------------------------------------------------------+
-| NVIDIA-SMI 396.15                 Driver Version: 396.15                    |
-|-------------------------------+----------------------+----------------------+
-| GPU  Name        Persistence-M| Bus-Id        Disp.A | Volatile Uncorr. ECC |
-| Fan  Temp  Perf  Pwr:Usage/Cap|         Memory-Usage | GPU-Util  Compute M. |
-|===============================+======================+======================|
-|   {{ .GpuIdx }}{{ .Space1 }}{{ .GpuProduct }}  Off  | 00000002:01:00.0 Off |                    0 |
-| N/A   30C    P0    38W / 300W |{{ .Space2 }}{{ .GpuUsedMem }}MiB / {{ .GpuTotalMem }}MiB{{ .Space3 }}|{{ .Space4 }}{{ .GpuUtil }}%      Default |
-+-------------------------------+----------------------+----------------------+
-                                                                               
-+-----------------------------------------------------------------------------+
-| Processes:                                                       GPU Memory |
-|  GPU       PID   Type   Process name                             Usage      |
-|=============================================================================|
-|    0    124097      C   {{ .ProcessName }}{{ .Space5 }}{{ .GpuUsedMem }}MiB{{ .Space6 }}|
-+-----------------------------------------------------------------------------+
-`
 
 type nvidiaSmiArgs struct {
 	GpuProduct  string
@@ -47,22 +30,7 @@ func main() {
 
 	args := getNvidiaSmiArgs()
 
-	log.Printf("%+v", args)
-
 	printArgs(args)
-	args = rightSizeArgs(args)
-
-	// t := template.New("nvidia-smi")
-
-	// t, err = t.Parse(nvidiaSmiOutputTemplate)
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// err = t.Execute(os.Stdout, args)
-	// if err != nil {
-	// 	panic(err)
-	// }
 }
 
 func getNvidiaSmiArgs() (args nvidiaSmiArgs) {
@@ -128,27 +96,14 @@ func getNvidiaSmiArgs() (args nvidiaSmiArgs) {
 		panic(err)
 	}
 
-	args.ProcessName = string(cmdlineBytes)
+	args.ProcessName = string(bytes.Trim(cmdlineBytes, "\x00"))
 
 	return args
 }
 
-// // simple table with zero customizations
-// tw := NewWriter()
-// // append a header row
-// tw.AppendHeader(Row{"#", "First Name", "Last Name", "Salary"})
-// // append some data rows
-// tw.AppendRows([]Row{
-// 	{1, "Arya", "Stark", 3000},
-// 	{20, "Jon", "Snow", 2000, "You know nothing, Jon Snow!"},
-// 	{300, "Tyrion", "Lannister", 5000},
-// })
-// // append a footer row
-// tw.AppendFooter(Row{"", "", "Total", 10000})
-// // render it
-// fmt.Printf("Table without any customizations:\n%s", tw.Render())
-
 func printArgs(args nvidiaSmiArgs) {
+	// Example:
+	//
 	// Wed Jun 29 14:19:35 2022
 	// +-----------------------------------------------------------------------------+
 	// | NVIDIA-SMI 470.129.06   Driver Version: 470.129.06   CUDA Version: 11.4     |
@@ -170,17 +125,19 @@ func printArgs(args nvidiaSmiArgs) {
 	// |    0   N/A  N/A       964      G   /usr/lib/xorg/Xorg                  4MiB |
 	// +-----------------------------------------------------------------------------+
 
+	// Print date
+	fmt.Println(time.Now().Format(time.ANSIC))
 	t := table.NewWriter()
 	t.SetOutputMirror(os.Stdout)
 	t.SetTitle("NVIDIA-SMI 470.129.06   Driver Version: 470.129.06   CUDA Version: 11.4")
 	t.AppendSeparator()
-	t.AppendRow(table.Row{"GPU  Name        Persistence-M", "Bus-Id        Disp.A", "Volatile   Uncorr. ECC"})
-	t.AppendRow(table.Row{"Fan  Temp  Perf  Pwr:Usage/Cap", "        Memory-Usage", "GPU-Util   Compute M."})
-	t.AppendRow(table.Row{"", "", "               MIG M."})
+	t.AppendRow(table.Row{"GPU  Name        Persistence-M", "Bus-Id        Disp.A", "Volatile Uncorr. ECC"})
+	t.AppendRow(table.Row{"Fan  Temp  Perf  Pwr:Usage/Cap", "        Memory-Usage", "GPU-Util  Compute M."})
+	t.AppendRow(table.Row{"", "", "              MIG M."})
 	t.AppendSeparator()
-	t.AppendRow(table.Row{fmt.Sprintf(" %d   %s           Off", args.GpuIdx, args.GpuProduct), "00000001:00:00.0 Off", " 			   Off"})
-	t.AppendRow(table.Row{" N/A   33C    P8    11W /  70W", fmt.Sprintf("  %dMiB / %dMiB", args.GpuTotalMem, args.GpuTotalMem), fmt.Sprintf("  %d%%        Default", args.GpuUtil)})
-	t.AppendRow(table.Row{"", "", "               N/A"})
+	t.AppendRow(table.Row{fmt.Sprintf("%s  %s%s", sizeString(strconv.Itoa(args.GpuIdx), 3, true), sizeString(args.GpuProduct, 12, false), sizeString("Off", 13, true)), fmt.Sprintf("%s %s", sizeString("00000001:00:00.0", 16, false), sizeString("Off", 3, true)), sizeString("Off", 20, true)})
+	t.AppendRow(table.Row{"N/A   33C    P8    11W /  70W", sizeString(fmt.Sprintf("%dMiB / %dMiB", args.GpuTotalMem, args.GpuTotalMem), 20, true), fmt.Sprintf("%s %s", sizeString(strconv.Itoa(args.GpuUtil)+"%", 8, true), sizeString("Default", 11, true))})
+	t.AppendRow(table.Row{"", "", sizeString("N/A", 20, true)})
 	t.AppendSeparator()
 	t.Render()
 
@@ -189,17 +146,25 @@ func printArgs(args nvidiaSmiArgs) {
 	t = table.NewWriter()
 	t.SetOutputMirror(os.Stdout)
 	t.AppendRow(table.Row{"Processes:"})
-	t.AppendRow(table.Row{"GPU   GI   CI            PID   Type   Process name                  GPU Memory"})
-	t.AppendRow(table.Row{"      ID   ID                                                       Usage      "})
+	t.AppendRow(table.Row{" GPU   GI   CI        PID   Type   Process name                  GPU Memory "})
+	t.AppendRow(table.Row{"       ID   ID                                                   Usage      "})
 	t.AppendSeparator()
-	t.AppendRow(table.Row{fmt.Sprintf(" %d   N/A  N/A             1      G    %s 				%dMiB", args.GpuIdx, args.ProcessName, int(args.GpuUsedMem))})
+	t.AppendRow(table.Row{fmt.Sprintf(" %s   %s%s%s%s   %s %s", sizeString(strconv.Itoa(args.GpuIdx), 3, true), sizeString("N/A", 5, false), sizeString("N/A", 10, false), sizeString(strconv.Itoa(os.Getpid()), 6, false), sizeString("G", 4, true), sizeString(args.ProcessName, 29, false), sizeString(fmt.Sprintf("%dMiB", int(args.GpuUsedMem)), 11, true))})
 	t.Render()
 }
 
-func rightSizeArgs(args nvidiaSmiArgs) nvidiaSmiArgs {
-	if len(args.GpuProduct) > 18 {
-		args.GpuProduct = args.GpuProduct[:15]
+func sizeString(str string, size int, alignRight bool) string {
+	if len(str) < size {
+		if alignRight {
+			str = strings.Repeat(" ", size-len(str)) + str
+		} else {
+			str = str + strings.Repeat(" ", size-len(str))
+		}
 	}
 
-	return args
+	if len(str) > size {
+		str = str[:size-2] + ".."
+	}
+
+	return str[:size]
 }
