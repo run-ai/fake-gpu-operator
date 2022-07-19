d44945e29652f8d008239f70cb01b07ed81cdc7a
Guy Shaibi <guy.shaibi@run.ai>
2022-07-19 13:43:58 +0300

    Random fix

--- internal/common/topology/podGpuUsageStatusMap.go
+++ internal/common/topology/podGpuUsageStatusMap.go
@@ -8,7 +8,7 @@ func (m *PodGpuUsageStatusMap) Utilization() int {
 		sum += v.Utilization.Random()
 	}
 
-	return int(math.Max(100, float64(sum)))
+	return int(math.Min(100, float64(sum)))
 }
 
 func (m *PodGpuUsageStatusMap) FbUsed(fbTotal int) int {
@@ -17,5 +17,5 @@ func (m *PodGpuUsageStatusMap) FbUsed(fbTotal int) int {
 		sum += v.FbUsed
 	}
 
-	return int(math.Max(float64(fbTotal), float64(sum)))
+	return int(math.Min(float64(fbTotal), float64(sum)))
 }
--- internal/common/topology/range.go
+++ internal/common/topology/range.go
@@ -1,7 +1,13 @@
 package topology
 
-import "math/rand"
+import (
+	"math/rand"
+)
 
 func (r *Range) Random() int {
+	if r.Min == r.Max {
+		return r.Min
+	}
+
 	return r.Min + rand.Intn(r.Max-r.Min)
 }
