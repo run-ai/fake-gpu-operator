dbd9f5456c6b8363a014b9911abc26b2d02356fa
Guy Shaibi <guy.shaibi@run.ai>
2022-07-19 13:26:03 +0300

    Add tests

--- internal/status-updater/app_test.go
+++ internal/status-updater/app_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"strconv"
 	"testing"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -26,14 +27,17 @@ import (
 )
 
 const (
-	topologyCmName      = "fake-cm-name"
-	topologyCmNamespace = "fake-cm-namespace"
-	podNamespace        = "fake-pod-namespace"
-	podName             = "fake-pod-name"
-	podUID              = "fake-pod-uid"
-	containerName       = "fake-container-name"
-	node                = "fake-node"
-	nodeGpuCount        = 2
+	topologyCmName              = "fake-cm-name"
+	topologyCmNamespace         = "fake-cm-namespace"
+	podNamespace                = "fake-pod-namespace"
+	podName                     = "fake-pod-name"
+	podUID                      = "fake-pod-uid"
+	containerName               = "fake-container-name"
+	reservationPodNs            = "runai-reservation"
+	reservationPodName          = "gpu-reservation-pod"
+	reservationPodContainerName = "reservation-pod-container"
+	node                        = "fake-node"
+	nodeGpuCount                = 2
 )
 
 func TestStatusUpdater(t *testing.T) {
@@ -89,29 +93,33 @@ var _ = Describe("StatusUpdater", func() {
 		})
 	})
 
-	When("informed of a GPU pod", func() {
+	When("informed of a dedicated GPU pod", func() {
 		type testCase struct {
-			podGpuCount int64
-			podPhase    v1.PodPhase
+			podGpuCount  int64
+			podPhase     v1.PodPhase
+			workloadType string
 		}
 
 		cases := []testCase{}
 
 		for i := int64(1); i <= nodeGpuCount; i++ {
 			for _, phase := range []v1.PodPhase{v1.PodPending, v1.PodRunning, v1.PodSucceeded, v1.PodFailed, v1.PodUnknown} {
-				cases = append(cases, testCase{
-					podGpuCount: i,
-					podPhase:    phase,
-				})
+				for _, workloadType := range []string{"build", "train", "interactive-preemptible"} {
+					cases = append(cases, testCase{
+						podGpuCount:  i,
+						podPhase:     phase,
+						workloadType: workloadType,
+					})
+				}
 			}
 		}
 
 		for _, caseDetails := range cases {
-			caseBaseName := fmt.Sprintf("GPU count %d, pod phase %s", caseDetails.podGpuCount, caseDetails.podPhase)
+			caseBaseName := fmt.Sprintf("GPU count %d, pod phase %s, workloadType: %s", caseDetails.podGpuCount, caseDetails.podPhase, caseDetails.workloadType)
 			It(caseBaseName, func() {
 				go status_updater.Run()
 
-				pod := createPod(caseDetails.podGpuCount, caseDetails.podPhase)
+				pod := createDedicatedGpuPod(caseDetails.podGpuCount, caseDetails.podPhase)
 				_, err := kubeclient.CoreV1().Pods(podNamespace).Create(context.TODO(), pod, metav1.CreateOptions{})
 				Expect(err).ToNot(HaveOccurred())
 
@@ -120,11 +128,8 @@ var _ = Describe("StatusUpdater", func() {
 					for i := 0; i < int(caseDetails.podGpuCount); i++ {
 						expectedTopology.Nodes[node].Gpus[i].Metrics.PodGpuUsageStatus = topology.PodGpuUsageStatusMap{
 							podUID: topology.GpuUsageStatus{
-								Utilization: topology.Range{
-									Min: 100,
-									Max: 100,
-								},
-								FbUsed: expectedTopology.Nodes[node].GpuMemory,
+								Utilization: getWorkloadTypeExpectedUtilization(caseDetails.workloadType),
+								FbUsed:      expectedTopology.Nodes[node].GpuMemory,
 							},
 						}
 						expectedTopology.Nodes[node].Gpus[i].Metrics.Metadata.Pod = podName
@@ -140,6 +145,40 @@ var _ = Describe("StatusUpdater", func() {
 				Eventually(getTopologyFromKube(kubeclient)).Should(Equal(createTopology(nodeGpuCount)))
 			})
 		}
+	})
+
+	When("informed of a shared GPU pod", func() {
+		It("should update the cluster topology at its reservation pod location", func() {
+			go status_updater.Run()
+
+			// Test reservation pod handling
+			reservationPod := createReservationPod(0)
+			_, err := kubeclient.CoreV1().Pods(reservationPodNs).Create(context.TODO(), reservationPod, metav1.CreateOptions{})
+			Expect(err).ToNot(HaveOccurred())
+
+			expectedTopology := createTopology(nodeGpuCount)
+			expectedTopology.Nodes[node].Gpus[0].Metrics.Metadata.Pod = reservationPodName
+			expectedTopology.Nodes[node].Gpus[0].Metrics.Metadata.Container = reservationPodContainerName
+			expectedTopology.Nodes[node].Gpus[0].Metrics.Metadata.Namespace = reservationPodNs
+			Eventually(getTopologyFromKube(kubeclient)).Should(Equal(expectedTopology))
+
+			// Test shared gpu pod handling
+			pod := createSharedGpuPod(0, 0.5, v1.PodRunning)
+			_, err = kubeclient.CoreV1().Pods(podNamespace).Create(context.TODO(), pod, metav1.CreateOptions{})
+			Expect(err).ToNot(HaveOccurred())
+
+			expectedTopology.Nodes[node].Gpus[0].Metrics.PodGpuUsageStatus = topology.PodGpuUsageStatusMap{
+				podUID: topology.GpuUsageStatus{
+					Utilization: topology.Range{
+						Min: 100,
+						Max: 100,
+					},
+					FbUsed: int(float64(expectedTopology.Nodes[node].GpuMemory) * 0.5),
+				},
+			}
+
+			Eventually(getTopologyFromKube(kubeclient)).Should(Equal(expectedTopology))
+		})
 	})
 })
 
@@ -194,7 +233,7 @@ func createTopology(gpuCount int64) *topology.ClusterTopology {
 	}
 }
 
-func createPod(gpuCount int64, phase v1.PodPhase) *v1.Pod {
+func createDedicatedGpuPod(gpuCount int64, phase v1.PodPhase) *v1.Pod {
 	return &v1.Pod{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      podName,
@@ -217,5 +256,78 @@ func createPod(gpuCount int64, phase v1.PodPhase) *v1.Pod {
 		Status: v1.PodStatus{
 			Phase: phase,
 		},
+	}
+}
+
+func createSharedGpuPod(gpuIdx int, gpuFraction float64, phase v1.PodPhase) *v1.Pod {
+	return &v1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      podName,
+			Namespace: podNamespace,
+			UID:       podUID,
+			Annotations: map[string]string{
+				"runai-gpu":    strconv.Itoa(gpuIdx),
+				"gpu-fraction": fmt.Sprintf("%f", gpuFraction),
+			},
+		},
+		Spec: v1.PodSpec{
+			NodeName: node,
+			Containers: []v1.Container{
+				{
+					Name: containerName,
+				},
+			},
+		},
+		Status: v1.PodStatus{
+			Phase: phase,
+		},
+	}
+}
+
+func createReservationPod(gpuIdx int) *v1.Pod {
+	return &v1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      reservationPodName,
+			Namespace: reservationPodNs,
+			Annotations: map[string]string{
+				"run.ai/reserve_for_gpu_index": strconv.Itoa(gpuIdx),
+			},
+		},
+		Spec: v1.PodSpec{
+			NodeName: node,
+			Containers: []v1.Container{
+				{
+					Name: reservationPodContainerName,
+					Resources: v1.ResourceRequirements{
+						Limits: v1.ResourceList{
+							"nvidia.com/gpu": *resource.NewQuantity(1, resource.DecimalSI),
+						},
+					},
+				},
+			},
+		},
+		Status: v1.PodStatus{
+			Phase: v1.PodRunning,
+		},
+	}
+}
+
+func getWorkloadTypeExpectedUtilization(workloadType string) topology.Range {
+	switch workloadType {
+	case "train":
+		return topology.Range{
+			Min: 80,
+			Max: 100,
+		}
+	case "build", "interactive-preemptible":
+		return topology.Range{
+			Min: 0,
+			Max: 0,
+		}
+	default:
+		return topology.Range{
+			Min: 100,
+			Max: 100,
+		}
 	}
 }
