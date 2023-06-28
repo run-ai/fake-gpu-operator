9ff0914db3515eb75ccad81938aeb6ac7306074e
Viktor Koukouliev <viktor.koukouliev@run.ai>
2023-06-28 17:14:36 +0300

    topology server

--- cmd/topology-server/main.go
+++ cmd/topology-server/main.go
@@ -5,14 +5,17 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/run-ai/fake-gpu-operator/internal/common/kubeclient"
 	"github.com/run-ai/fake-gpu-operator/internal/common/topology"
+	"github.com/spf13/viper"
 )
 
 func main() {
 	kubeclient := kubeclient.NewKubeClient(nil, nil)
-
+	viper.SetDefault("TOPOLOGY_CM_NAME", os.Getenv("TOPOLOGY_CM_NAME"))
+	viper.SetDefault("TOPOLOGY_CM_NAMESPACE", os.Getenv("TOPOLOGY_CM_NAMESPACE"))
 	http.HandleFunc("/topology", func(w http.ResponseWriter, r *http.Request) {
 		cm, ok := kubeclient.GetConfigMap(os.Getenv("TOPOLOGY_CM_NAMESPACE"), os.Getenv("TOPOLOGY_CM_NAME"))
 		if !ok {
@@ -32,6 +35,31 @@ func main() {
 
 		w.Header().Set("Content-Type", "application/json")
 		_, err = w.Write(clusterTopologyJSON)
+		if err != nil {
+			panic(err)
+		}
+	})
+
+	http.HandleFunc("/topology/nodes/", func(w http.ResponseWriter, r *http.Request) {
+		nodeName := strings.Split(r.URL.Path, "/")[3]
+		cm, ok := kubeclient.GetConfigMap(os.Getenv("TOPOLOGY_CM_NAMESPACE"), topology.GetNodeTopologyCMName(nodeName))
+		if !ok {
+			panic("Can't get node topology for node " + nodeName)
+		}
+		nodeTopology, err := topology.FromNodeConfigMap(cm)
+		if err != nil {
+			panic(err)
+		}
+
+		nodeTopologyJSON, err := json.Marshal(nodeTopology)
+		if err != nil {
+			panic(err)
+		}
+
+		log.Printf("Returning node topology: %s", nodeTopologyJSON)
+
+		w.Header().Set("Content-Type", "application/json")
+		_, err = w.Write(nodeTopologyJSON)
 		if err != nil {
 			panic(err)
 		}
