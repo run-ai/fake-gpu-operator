72c7319d45caae6915516c8dfe2a73b8d77a60e9
Viktor Koukouliev <viktor.koukouliev@run.ai>
2023-07-02 17:19:54 +0300

    return errors instead of panic

--- cmd/topology-server/main.go
+++ cmd/topology-server/main.go
@@ -10,6 +10,7 @@ import (
 	"github.com/run-ai/fake-gpu-operator/internal/common/kubeclient"
 	"github.com/run-ai/fake-gpu-operator/internal/common/topology"
 	"github.com/spf13/viper"
+	"k8s.io/apimachinery/pkg/api/errors"
 )
 
 func main() {
@@ -42,26 +43,32 @@ func main() {
 
 	http.HandleFunc("/topology/nodes/", func(w http.ResponseWriter, r *http.Request) {
 		nodeName := strings.Split(r.URL.Path, "/")[3]
+		w.Header().Set("Content-Type", "application/json")
+
 		if nodeName == "" {
 			panic("Can't get node name from url " + r.URL.Path)
 		}
-		cm, ok := kubeclient.GetConfigMap(os.Getenv("TOPOLOGY_CM_NAMESPACE"), topology.GetNodeTopologyCMName(nodeName))
-		if !ok {
-			panic("Can't get node topology for node " + nodeName)
-		}
-		nodeTopology, err := topology.FromNodeTopologyCM(cm)
+		nodeTopology, err := topology.GetNodeTopologyFromCM(kubeclient.ClientSet, nodeName)
 		if err != nil {
-			panic(err)
+			if errors.IsNotFound(err) {
+				w.WriteHeader(http.StatusNotFound)
+				w.Write([]byte("Node topology not found"))
+			} else {
+				w.WriteHeader(http.StatusInternalServerError)
+				w.Write([]byte(err.Error()))
+			}
+			return
 		}
 
 		nodeTopologyJSON, err := json.Marshal(nodeTopology)
 		if err != nil {
-			panic(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
 		}
 
 		log.Printf("Returning node topology: %s", nodeTopologyJSON)
 
-		w.Header().Set("Content-Type", "application/json")
 		_, err = w.Write(nodeTopologyJSON)
 		if err != nil {
 			panic(err)
