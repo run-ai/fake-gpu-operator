3f563a856ffca69c5b6393c7a6913d63ad0db2cf
Erez Freiberger <enoodle@gmail.com>
2026-01-12 12:50:03 +0100

    add compute domain DRA plugin skeleton
    Add the infrastructure for a compute domain DRA plugin that registers
    with kubelet, publishes resource slices for channel devices, but does
    not yet implement actual device preparation (Prepare/Unprepare are stubs).
    
    This includes:
    - cmd/compute-domain-dra-plugin entry point
    - internal/compute-domain-dra-plugin with app, driver, and discovery logic
    - Helm templates for DaemonSet, RBAC, and DeviceClass
    - Dockerfile and Makefile updates

---
+++ internal/compute-domain-dra-plugin/app.go
@@ -0,0 +1,180 @@
+/*
+ * Copyright 2025 The Kubernetes Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package computedomaindraplugin
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"syscall"
+
+	"github.com/run-ai/fake-gpu-operator/internal/common/app"
+	"github.com/run-ai/fake-gpu-operator/pkg/compute-domain/consts"
+	"go.uber.org/zap/zapcore"
+	coreclientset "k8s.io/client-go/kubernetes"
+	"k8s.io/dynamic-resource-allocation/kubeletplugin"
+	"k8s.io/klog/v2"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	"sigs.k8s.io/dra-example-driver/pkg/flags"
+)
+
+type Flags struct {
+	kubeClientConfig flags.KubeClientConfig
+	loggingConfig    *flags.LoggingConfig
+
+	nodeName                      string
+	cdiRoot                       string
+	kubeletRegistrarDirectoryPath string
+	kubeletPluginsDirectoryPath   string
+}
+
+type Config struct {
+	flags         *Flags
+	coreclient    coreclientset.Interface
+	cancelMainCtx func(error)
+}
+
+func (c Config) DriverPluginPath() string {
+	return filepath.Join(c.flags.kubeletPluginsDirectoryPath, consts.ComputeDomainDriverName)
+}
+
+type AppConfig struct {
+	NodeName                      string `mapstructure:"NODE_NAME"`
+	CDIRoot                       string `mapstructure:"CDI_ROOT"`
+	KubeletRegistrarDirectoryPath string `mapstructure:"KUBELET_REGISTRAR_DIRECTORY_PATH"`
+	KubeletPluginsDirectoryPath   string `mapstructure:"KUBELET_PLUGINS_DIRECTORY_PATH"`
+}
+
+type ComputeDomainDRAPluginApp struct {
+	config *AppConfig
+	stop   chan struct{}
+}
+
+func NewComputeDomainDRAPluginApp() *ComputeDomainDRAPluginApp {
+	return &ComputeDomainDRAPluginApp{}
+}
+
+func (app *ComputeDomainDRAPluginApp) Name() string {
+	return "ComputeDomainDRAPlugin"
+}
+
+func (app *ComputeDomainDRAPluginApp) GetConfig() interface{} {
+	if app.config == nil {
+		app.config = &AppConfig{
+			CDIRoot:                       "/etc/cdi",
+			KubeletRegistrarDirectoryPath: kubeletplugin.KubeletRegistryDir,
+			KubeletPluginsDirectoryPath:   kubeletplugin.KubeletPluginsDir,
+		}
+	}
+	return app.config
+}
+
+func (app *ComputeDomainDRAPluginApp) Init(stop chan struct{}) {
+	app.stop = stop
+}
+
+func (app *ComputeDomainDRAPluginApp) Run() {
+	opts := zap.Options{
+		Development: true,
+		TimeEncoder: zapcore.ISO8601TimeEncoder,
+	}
+	klog.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	go func() {
+		<-app.stop
+		cancel()
+	}()
+
+	if err := app.runPlugin(ctx); err != nil {
+		klog.Error(err, "plugin exited with error")
+	}
+}
+
+func (app *ComputeDomainDRAPluginApp) runPlugin(ctx context.Context) error {
+	logger := klog.FromContext(ctx)
+
+	if app.config.NodeName == "" {
+		return fmt.Errorf("NODE_NAME is required")
+	}
+
+	kubeClientConfig := flags.KubeClientConfig{}
+	clientSets, err := kubeClientConfig.NewClientSets()
+	if err != nil {
+		return fmt.Errorf("create client: %v", err)
+	}
+
+	config := &Config{
+		flags: &Flags{
+			kubeClientConfig:              kubeClientConfig,
+			loggingConfig:                 flags.NewLoggingConfig(),
+			nodeName:                      app.config.NodeName,
+			cdiRoot:                       app.config.CDIRoot,
+			kubeletRegistrarDirectoryPath: app.config.KubeletRegistrarDirectoryPath,
+			kubeletPluginsDirectoryPath:   app.config.KubeletPluginsDirectoryPath,
+		},
+		coreclient: clientSets.Core,
+	}
+
+	err = os.MkdirAll(config.DriverPluginPath(), 0750)
+	if err != nil {
+		return err
+	}
+
+	info, err := os.Stat(config.flags.cdiRoot)
+	switch {
+	case err != nil && os.IsNotExist(err):
+		err := os.MkdirAll(config.flags.cdiRoot, 0750)
+		if err != nil {
+			return err
+		}
+	case err != nil:
+		return err
+	case !info.IsDir():
+		return fmt.Errorf("path for cdi file generation is not a directory: '%v'", config.flags.cdiRoot)
+	}
+
+	ctx, stop := signal.NotifyContext(ctx, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	defer stop()
+	ctx, cancel := context.WithCancelCause(ctx)
+	config.cancelMainCtx = cancel
+
+	driver, err := NewComputeDomainDriver(ctx, config)
+	if err != nil {
+		return err
+	}
+
+	<-ctx.Done()
+	stop()
+	if err := context.Cause(ctx); err != nil && !errors.Is(err, context.Canceled) {
+		logger.Error(err, "error from context")
+	}
+
+	err = driver.Shutdown(logger)
+	if err != nil {
+		logger.Error(err, "Unable to cleanly shutdown driver")
+	}
+
+	return nil
+}
+
+var _ app.App = &ComputeDomainDRAPluginApp{}
